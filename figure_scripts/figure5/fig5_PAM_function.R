35d194884ecdec57ab4bf724f66f0941d6983204
nicolas <nicolas.arias.vaccari@gmail.com>
2023-08-31 18:49:08 -0500

    completed hill function within PAM

--- figure_scripts/figure5/fig5_PAM_function.R
+++ figure_scripts/figure5/fig5_PAM_function.R
@@ -4,12 +4,12 @@ library(tidyverse)
 # Create a function for each possible parameter modification 
 func_PAM <- function(iptg, g=17.129, k2=0.1, b=2791.209,
                      k1 = 0.1, kf=0.1, kr=0.1, h=0.2, 
-                     ki=0.02, q=0.45, R0=0.06){
+                     ki=0.02, n=4, q=0.45, R0=0.06){
   
   FAM_wt <- function(t, state, parameters) {
     with(as.list(c(state, parameters)), {
       
-      fH <- h*(Ii/(Ii+ki))*(kf/(x+kf))
+      fH <- h*((Ii^n)/((Ii^n)+(ki^n)))
       fR <- (1-q)*(x/(x+kr))
       fC <- (1-q)*(kf/(x+kf))
       fQ <- q
@@ -34,7 +34,7 @@ func_PAM <- function(iptg, g=17.129, k2=0.1, b=2791.209,
   
   parameters <- c(g = g, k2 = k2, b = b, R0=R0,
                   k1 = k1, kf = kf, h = h, kr=kr,
-                  q = q, ki = ki)
+                  q = q, ki = ki, n = n)
   state      <- c(Ie = iptg, Ii = 0, N = 10^7,S = 2, x = 0, 
                   C = 0.05, R = 0.1, H = 0, Q = 0.1)
   times      <- seq(0, 16, by = 0.01)
@@ -48,12 +48,12 @@ func_PAM <- function(iptg, g=17.129, k2=0.1, b=2791.209,
 # respuesta a estres hipotetica
 func_stress_PAM <- function(iptg, g=17.129, k2=0.1, b=2791.209,
                             k1 = 0.1, kf=0.1, kr=0.1, h=0.2, m =0.2,
-                            ki=0.02, q=0.45, R0=0.06){
+                            ki=0.02, n=4, q=0.45, R0=0.06){
   
   FAM_wt <- function(t, state, parameters) {
     with(as.list(c(state, parameters)), {
       
-      fH <- h*(Ii/(Ii+ki))*(kf/(x+kf))
+      fH <- h*((Ii^n)/((Ii^n)+(ki^n)))
       fM <- m*(H/(H+0.1))
       fR <- (1-q)*(x/(x+kr))
       fC <- (1-q)*(kf/(x+kf))
@@ -82,7 +82,7 @@ func_stress_PAM <- function(iptg, g=17.129, k2=0.1, b=2791.209,
   
   parameters <- c(g = g, k2 = k2, b = b, R0=R0,
                   k1 = k1, kf = kf, h = h, kr=kr,
-                  q = q, ki = ki)
+                  q = q, ki = ki, n = n)
   state      <- c(Ie = iptg, Ii = 0, N = 10^7,S = 2, x = 0, 
                   C = 0.05, R = 0.1, H = 0, Q = 0.1, M = 0)
   times      <- seq(0, 16, by = 0.01)
